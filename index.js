b9bef0620dd1bf7f250e318b64a0725dc4a3173d
Mohamed Noor <mohamednoordagane2195@gmail.com>
2026-01-21 21:46:54 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+
+// employee object required by the tests
+const employee = {
+  name: "John",
+  age: 30,
+  role: "Developer"
+};
+
+// NON-destructive update
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return Object.assign({}, employee, { [key]: value });
+}
+// Destructive update
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+// NON-destructive delete
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = Object.assign({}, employee);
+  delete newEmployee[key];
+  return newEmployee;
+}
+// Destructive delete
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
